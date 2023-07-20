a469c30122489e78ad0ec7ec90e1fde77e09ddc0
Amogh Jambekar <amoghjambekar45@gmail.com>
2023-07-20 11:00:16 -0400

    removed bar and fixed generation

--- script.js
+++ script.js
@@ -22,24 +22,54 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const newPassword = generateNewPassword(originalPassword);
     newPasswordInput.value = newPassword;
     
-    const strengthPercentage = calculatePasswordStrength(newPassword);
-    passwordStrengthBar.style.width = `${strengthPercentage}%`;
-    
     const time = calculateTimeToCrack(newPassword);
-    timeToCrack.textContent = `Approx. time to crack: ${time} seconds`;
+    timeToCrack.textContent = `Approx. time to crack: ${Math.round(time/10000000000)} days`;
     timeToCrackContainer.style.display = "block"; // Show the time-to-crack container
   });
 
   // Helper functions to generate a new password, calculate password strength, and time to crack
   function generateNewPassword(originalPassword) {
-    // Implement your password generation logic here
+    const specialCharacters = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+    const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+  
+    // Combine all characters to choose from
+    const allCharacters = upperCaseLetters + lowerCaseLetters + numbers + specialCharacters;
+  
+    let newPassword = "";
+  
+    // Function to generate a random number between min and max (both inclusive)
+    const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+  
+    for (const char of originalPassword) {
+      newPassword += char;
+      const numberOfRandomCharacters = getRandomInt(1, 3); // Adjust the range as needed (1 to 3 in this example)
+      for (let i = 0; i < numberOfRandomCharacters; i++) {
+        const randomCharacter = allCharacters[Math.floor(Math.random() * allCharacters.length)];
+        newPassword += randomCharacter;
+      }
+    }
+  
+    return newPassword;
   }
 
-  function calculatePasswordStrength(password) {
-    // Implement your password strength calculation logic here
-  }
 
   function calculateTimeToCrack(password) {
-    // Implement your password cracking time calculation logic here
+    const attemptsPerSecond = 1000000; // Arbitrary number of attempts per second
+    const secondsInDay = 86400; // Number of seconds in a day
+  
+    // Calculate the total number of possible combinations for a brute-force attack
+    const characterSet = 94; // Number of possible characters (alphanumeric + special characters)
+    const passwordLength = password.length;
+    const possibleCombinations = Math.pow(characterSet, passwordLength);
+  
+    // Calculate the time to crack in seconds
+    const timeToCrackInSeconds = possibleCombinations / attemptsPerSecond;
+  
+    // Convert time to crack to days
+    const timeToCrackInDays = timeToCrackInSeconds / secondsInDay;
+  
+    return timeToCrackInDays ;
   }
 });
